ca3089194fd60896b75314b49c3de12571a5aee2
stella17wgit config --global user.email stellaw813@gmail.com git config --global user.name stella17w <stellaw813@gmail.com>
2024-04-20 00:00:10 -0400

    Add some improvements

--- NN.py
+++ NN.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 #dictionary to convert strings into numberic values
 radio_mod={'BPSK':0,'QPSK':1,'8PSK':2,'QAM16':3,'QAM64':4,'BFSK':5,'CPFSK':6,'PAM4':7,'GFSK':8,'WBFM':9,'AM-SSB':10,'AM-DSB':11}
@@ -13,13 +14,16 @@ class RadioNN(nn.Module):
     def __init__(self):
         super().__init__()
         self.hidden1=nn.InstanceNorm1d(2)
-        self.conv1=nn.Conv1d(2,64,11)
+        self.conv1=nn.Conv1d(2,64,9)
         self.act1=nn.ReLU()
         self.hidden2=nn.InstanceNorm1d(64)
-        self.conv2=nn.Conv1d(64,2,11)
+        self.conv2=nn.Conv1d(64,64,9)
         self.act2=nn.ReLU()
-        self.hidden3=nn.InstanceNorm1d(2)
-        self.linear1=nn.Linear(108,64)
+        self.hidden3=nn.InstanceNorm1d(64)
+        self.conv3=nn.Conv1d(64,64,9)
+        self.act3=nn.ReLU()
+        self.hidden4=nn.InstanceNorm1d(64)
+        self.linear1=nn.Linear(104,64)
         self.linear2=nn.Linear(64,256)
         self.linear3=nn.Linear(256,2)
         self.pool=nn.MaxPool1d(1)
@@ -28,7 +32,8 @@ class RadioNN(nn.Module):
     def forward(self, x):
         x = self.act1(self.conv1(self.hidden1(x)))
         x = self.act2(self.conv2(self.hidden2(x)))
-        x = self.linear3(self.linear2(self.linear1(self.hidden3(x))))
+        x = self.act3(self.conv3(self.hidden3(x)))
+        x = self.linear3(self.linear2(self.linear1(self.hidden4(x))))
         x = self.output(self.pool(x))
         return x [:, -1, :]
 
@@ -36,9 +41,21 @@ model=RadioNN()
 
 #~Training
 
+#reset parameters
+model.hidden1.reset_parameters()
+model.conv1.reset_parameters()
+model.hidden2.reset_parameters()
+model.conv2.reset_parameters()
+model.hidden3.reset_parameters()
+model.conv3.reset_parameters()
+model.hidden4.reset_parameters()
+model.linear1.reset_parameters()
+model.linear2.reset_parameters()
+model.linear3.reset_parameters()
+
 #loss and optimizer
 fn_loss=nn.CrossEntropyLoss() #cross entropy
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=0.0009)
 
 #load training data
 with open("training.pkl", 'rb') as file:
@@ -49,8 +66,12 @@ x_train=[]
 y_train=[]
 for k in keys:
     for x in train_data[k]:
-        x_train.append(x)
-        y_train.append([radio_mod[k[0].decode()],k[1]])
+        if len(x_train)<2:
+            rand_num=0
+        else:
+            rand_num=random.randint(0,len(x_train)-1)
+        x_train.insert(rand_num,x)
+        y_train.insert(rand_num,[radio_mod[k[0].decode()],k[1]])
 x_train=torch.Tensor(np.array(x_train))
 y_train=torch.Tensor(np.array(y_train))
 
@@ -63,8 +84,12 @@ x_valid=[]
 y_valid=[]
 for k in keys:
     for x in valid_data[k]:
-        x_valid.append(x)
-        y_valid.append([radio_mod[k[0].decode()],k[1]])
+        if len(x_valid)<2:
+            rand_num=0
+        else:
+            rand_num=random.randint(0,len(x_valid)-1)
+        x_valid.insert(rand_num,x)
+        y_valid.insert(rand_num,[radio_mod[k[0].decode()],k[1]])
 x_valid=torch.Tensor(np.array(x_valid))
 y_valid=torch.Tensor(np.array(y_valid))
 
@@ -128,14 +153,18 @@ x_test=[]
 y_test=[]
 for k in keys:
     for x in test_data[k]:
-        x_test.append(x)
-        y_test.append([radio_mod[k[0].decode()],k[1]])
+        if len(x_test)<2:
+            rand_num=0
+        else:
+            rand_num=random.randint(0,len(x_test)-1)
+        x_test.insert(rand_num,x)
+        y_test.insert(rand_num,[radio_mod[k[0].decode()],k[1]])
 x_test=torch.Tensor(np.array(x_test))
 y_test=torch.Tensor(np.array(y_test))
 
 y_pred = model(x_test[0:len(x_test)])
 final_accurary=(y_pred.round() == y_test).float().mean()*100
-print("Final accuuary:",final_accurary.float())
+print("Final accuary:",final_accurary.float())
 
 #delete model
 del model
